0f05061572853bc8438d2eaf8c296c1fa7d87a96
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-01-09 00:07:17 +0800

    Update master_setup.sh

--- scripts_for_FIRM/master_setup.sh
+++ scripts_for_FIRM/master_setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# master node
-short_hostname=$(hostname --short)
+## master node
+# short_hostname=$(hostname --short)
+## mount /dev/sda4 to ~/workspace   that's for CloudLab
+sudo mkfs.ext4 /dev/sda4
+sudo mount /dev/sda4 ~
 
 ## https://gitlab.engr.illinois.edu/DEPEND/firm/-/blob/master/setup-k8s.md
 # Install Docker
@@ -47,3 +50,33 @@ make -j
 sudo make install
 
 # Deploy tracing, metrics exporting, collection agents:
+cd ~/firm
+export NAMESPACE='monitoring'
+kubectl create -f manifests/setup
+kubectl create namespace observability
+kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/v1.27.0/deploy/crds/jaegertracing.io_jaegers_crd.yaml
+kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/v1.27.0/deploy/service_account.yaml
+kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/v1.27.0/deploy/role.yaml
+kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/v1.27.0/deploy/role_binding.yaml
+kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/v1.27.0/deploy/operator.yaml
+kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/v1.27.0/deploy/cluster_role.yaml
+kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/v1.27.0/deploy/cluster_role_binding.yaml
+kubectl create -f manifests/
+# Deploy graph database and pipeline for metrics storage (this requires docker-compose to be installed and enabled on the master node):
+cd trace-grapher
+# sudo apt install -y docker-compose  <waiting to check>
+sudo docker-compose run stack-builder # need to modify version to 3.3
+# now a shell pops as root in the project directory of the stack-builder container
+cd deploy-trace-grapher
+make prepare-trace-grapher-namespace
+make install-components # cannot execute 
+# Install deployment module:
+exit # leave container
+cd ~/firm/third-party/intel-cmt-cat
+sudo make install # you need to make intel-cmt-cat otherwise there is no libs of intel-cmt-cat in /lib
+cd ~/firm/scripts   # because author's 
+make all
+# need to install virtualenv before "make env"
+pip3 install virtualenv
+cd python-cat-mba
+make env    # I remove "make -C ../../lib/python install; \"  it is weird
