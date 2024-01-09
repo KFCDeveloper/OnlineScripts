d6acaf080a316a6486db50ef6c27d5b8b16f19c4
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-01-09 16:21:07 +0800

    Update dqn_config.sh

--- script_DQN/dqn_config.sh
+++ script_DQN/dqn_config.sh
@@ -2,6 +2,6 @@
 ## master node
 sudo mkfs.ext4 /dev/sda4
 sudo mount /dev/sda4 ~
-sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo add-apt-repository -y ppa:graphics-drivers/ppa
 sudo apt-get update
 sudo apt-get install -y nvidia-driver-470
