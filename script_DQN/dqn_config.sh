6b49191cc9a073b613b525d515d455732d82facb
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-01-09 15:22:37 +0800

    Create dqn_config.sh

---
+++ script_DQN/dqn_config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+## master node
+sudo mkfs.ext4 /dev/sda4
+sudo mount /dev/sda4 ~
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+sudo apt-get install -y nvidia-driver-470
