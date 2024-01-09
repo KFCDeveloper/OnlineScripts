d2c587ce70f75dbb93358b05f361e5a374f2307a
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-01-09 20:38:23 +0800

    Update master_setup.sh

--- scripts_for_FIRM/master_setup.sh
+++ scripts_for_FIRM/master_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ## master node
 
-sudo su DylanYu # **** change user name ****
+# sudo su DylanYu # **** change user name ****
+export HOME="/users/DylanYu"
 
 ## mount /dev/sda4 to ~/workspace   that's for CloudLab
 sudo mkfs.ext4 /dev/sda4
