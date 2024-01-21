fafdede42f4f77bc226b7d3968c5f676d92b8941
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-01-21 21:53:32 +0800

    update orphan message

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # OnlineScripts
 Online scripts for projects
+Remeber to use ``git checkout --orphan xxx`` when you want to add a new project in the Album.
+After you ``git clone`` the repository, ``git checkout -t origin/xxx`` and ``git pull`` to get xxx project code.
--- script_MB2/mb2_setup.sh
+++ script_MB2/mb2_setup.sh
@@ -4,3 +4,14 @@
 cd /mydata
 sudo chmod -R  777 /mydata
 sudo chmod -R  777 ~
+
+# install noise page
+cd /mydata
+git clone https://github.com/cmu-db/noisepage.git
+cd noisepage
+sudo ./script/installation/packages.sh
+mkdir build
+cd build
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DNOISEPAGE_USE_JEMALLOC=ON -DNOISEPAGE_UNITY_BUILD=ON ..
+ninja noisepage
+./bin/noisepage
