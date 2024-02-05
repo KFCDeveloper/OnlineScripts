c0b4cd803ac9753023ce21c0301c700eb1ac6bc0
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-02-05 15:05:49 +0800

    update

--- script_CausalSim/causalsim_setup.sh
+++ script_CausalSim/causalsim_setup.sh
@@ -32,14 +32,14 @@ mv ML4SysReproduceProjects Unbiased-Trace-Driven-Simulation
 cd Unbiased-Trace-Driven-Simulation
 git checkout -t origin/Unbiased-Trace-Driven-Simulation
 # rsync -avz --progress -e ssh /mydata/Unbiased-Trace-Driven-Simulation/abr-puffer/CAUSALSIM_DIR /mydata/Unbiased-Trace-Driven-Simulation/abr-puffer/CAUSALSIM_DIR-20-11-27 /mydata/Unbiased-Trace-Driven-Simulation/abr-puffer/CAUSALSIM_DIR-20-11-27 /mydata/Unbiased-Trace-Driven-Simulation/abr-puffer/CAUSALSIM_DIR-21-1-27 DylanYu@c4130-110233.wisc.cloudlab.us:/mydata/Unbiased-Trace-Driven-Simulation/abr-puffer/
-# ln -s abr-puffer/CAUSALSIM_DIR-20-11-27/cooked abr-puffer/CAUSALSIM_DIR-21-3-27/
+# ln -s abr-puffer/CAUSALSIM_DIR-21-3-27/cooked abr-puffer/CAUSALSIM_DIR-21-3-27/
 
 # prepare dataset
 # ** ** 
 python3 data_preparation/create_dataset.py --dir CAUSALSIM_DIR-21-5-27/
 python data_preparation/generate_subset_data.py --dir CAUSALSIM_DIR-21-3-27/
 # train SLSim
-python training/sl_subset_train.py --dir CAUSALSIM_DIR-20-11-27/ --left_out_policy linear_bba  # python training/sl_subset_train.py --dir CAUSALSIM_DIR-20-11-27/ --left_out_policy target 
+python training/sl_subset_train.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy linear_bba  # python training/sl_subset_train.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy target 
 # train CasualSim
 python training/train_subset.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy linear_bba --C 0.05 
 
@@ -48,26 +48,30 @@ python training/train_subset.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy l
 python inference/extract_subset_latents.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy linear_bba --C 0.05 
 # Counterfactual Simulation
 python inference/expert_cfs.py --dir CAUSALSIM_DIR-21-3-27/ # ExpertSim ** need  (pretty time-consuming)
-python inference/sl_subset_cfs.py --dir CAUSALSIM_DIR-20-11-27/ --left_out_policy linear_bba # SLSim 
+python inference/sl_subset_cfs.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy linear_bba # SLSim 
 python inference/buffer_subset_cfs.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy linear_bba --C 0.05 # CausalSim (pretty time-consuming)
 # Calculate the average SSIM using the ground-truth data
-python analysis/original_subset_ssim.py --dir CAUSALSIM_DIR-21-1-27/ --left_out_policy linear_bba # ExpertSim ** need
-python analysis/sl_subset_ssim.py --dir CAUSALSIM_DIR-20-11-27/ --left_out_policy linear_bba # SLSim 
-python analysis/subset_ssim.py --dir CAUSALSIM_DIR-20-11-27/ --left_out_policy linear_bba --C 0.05 # CausalSim ** need
+python analysis/original_subset_ssim.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy linear_bba # ExpertSim ** need
+python analysis/sl_subset_ssim.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy linear_bba # SLSim 
+python analysis/subset_ssim.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy linear_bba --C 0.05 # CausalSim ** need
 # Calculate the simulated buffer distribution's Earth Mover Distance (EMD) using the fround-truth data
 python analysis/subset_EMD.py --dir CAUSALSIM_DIR-20-11-27/ --left_out_policy linear_bba --C 0.05 # All{ExpertSim, SLSim, CausalSim} ** need
 # Tune CausalSim's hyper-parameters for buffer and SSIM prediction
-python analysis/tune_buffer_hyperparams.py --dir CAUSALSIM_DIR-20-11-27/
+python analysis/tune_buffer_hyperparams.py --dir CAUSALSIM_DIR-21-3-27/
 
 # CausalSim model to generate counterfactual downloadtime trajectories
 python inference/downloadtime_subset_cfs.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy linear_bba --C 0.05  # (pretty time-consuming)
 # calculate the average stall ratio
-python analysis/original_subset_stall.py --dir CAUSALSIM_DIR-20-11-27/ --left_out_policy linear_bba # ExpertSim 
-python analysis/sl_subset_stall.py --dir CAUSALSIM_DIR-20-11-27/ --left_out_policy linear_bba  # SLSim 
-python analysis/subset_stall.py --dir CAUSALSIM_DIR-20-11-27/ --left_out_policy linear_bba --C 0.05 # CausalSim 
+python analysis/original_subset_stall.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy linear_bba # ExpertSim 
+python analysis/sl_subset_stall.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy linear_bba  # SLSim 
+python analysis/subset_stall.py --dir CAUSALSIM_DIR-21-3-27/ --left_out_policy linear_bba --C 0.05 # CausalSim 
 # Tune CausalSim's hyper-parameters for downloadtime prediction
-python analysis/tune_downloadtime_hyperparameters.py --dir CAUSALSIM_DIR-20-11-27/  
+python analysis/tune_downloadtime_hyperparameters.py --dir CAUSALSIM_DIR-21-3-27/  
 
 
 # draw Fig4
-python visualization/fig4_causal_only.py --dir CAUSALSIM_DIR-20-11-27/
+python visualization/fig4_causal_only.py --dir CAUSALSIM_DIR-21-3-27/
+# draw Fig7a
+python visualization/fig7a_causal_only.py --dir CAUSALSIM_DIR/
+
+python visualization/fig7a_conbine.py --dir CAUSALSIM_DIR/  # do not change dir
--- script_DOTE/dote_setup.sh
+++ script_DOTE/dote_setup.sh
@@ -85,8 +85,8 @@ wget http://www.topology-zoo.org/files/Abilene.gml
 # /networking_envs/data/gml_to_dote.py, set the src_dir, dest_dir and network_name variables.
 python3 gml_to_dote.py
 # don't forget!! To compute the optimum for the demand matrices, go to /mydata/DOTE/networking_envs/data/Abilene and run /mydata/DOTE/networking_envs/data/compute_opts.py
-cd /mydata/DOTE/networking_envs/data/Abilene
-python3 /mydata/DOTE/networking_envs/data/compute_opts.py   
+cd /mydata/DOTE/networking_envs/data/Abilene-0.75
+python3 /mydata/DOTE/networking_envs/data/compute_opts.py
 
 
 # install java8
@@ -96,9 +96,9 @@ sudo apt install openjdk-8-jdk -y
 
 # DOTE training execution code
 # python3 /mydata/DOTE/dote.py --ecmp_topo Abilene --paths_from sp --so_mode train --so_epochs 50 --so_batch_size 200 --opt_function MAXFLOW
-python3 /mydata/DOTE/dote.py --ecmp_topo Abilene-cut-node --paths_from sp --so_mode train --so_epochs 20 --so_batch_size 32 --opt_function MAXUTIL
+python3 /mydata/DOTE/dote.py --ecmp_topo Abilene-0.25 --paths_from sp --so_mode test --so_epochs 20 --so_batch_size 32 --opt_function MAXUTIL
 # test
-python3 /mydata/DOTE/dote.py --ecmp_topo Abilene-cut-node --paths_from sp --so_mode train --so_epochs 20 --so_batch_size 32 --opt_function MAXUTIL
+python3 /mydata/DOTE/dote.py --ecmp_topo Abilene --paths_from sp --so_mode train --so_epochs 20 --so_batch_size 32 --opt_function MAXUTIL
 
 # DOTE predict execution code
 # train: why first run this, than the latter; I find 1.processing training data 2.start training ;
