42b5deb5d75fd963bbc8a21c971b87d0b1c577f4
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-02-16 16:45:17 +0800

    update

--- script_Sinan/social_network_setup.sh
+++ script_Sinan/social_network_setup.sh
@@ -23,13 +23,11 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
+
 sudo apt-get update
 
 sudo apt-get install docker-ce=5:19.03.15~3-0~ubuntu-bionic docker-ce-cli=5:19.03.15~3-0~ubuntu-bionic containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
 # install docker compose
-sudo apt-get install docker-compose-plugin
-sudo apt install docker-compose
 #
 sudo apt-get install libssl-dev -y
 sudo apt-get install libz-dev -y
