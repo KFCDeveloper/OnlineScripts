15fcdf813622442dedbcf305ecdd0936f0bd0955
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-02-17 01:20:03 +0800

    update

--- script_Sinan/sinan_setup.sh
+++ script_Sinan/sinan_setup.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+cd /mydata
+git clone https://github.com/zyqCSL/sinan-local.git
--- script_Sinan/social_network_setup.sh
+++ script_Sinan/social_network_setup.sh
@@ -2,8 +2,11 @@
 
 # https://ken.io/note/docker-swarm-cluster-setup-and-manage; compose cluster
 # *** manager
-sudo docker swarm init --advertise-addr=10.0.10.131
+sudo docker swarm init --advertise-addr=128.105.144.182 # ifconfig
 docker swarm join-token worker # obtain instruction of worker node joining
 # docker swarm init --advertise-addr=c220g5-111012.wisc.cloudlab.us --force-new-cluster
 # *** worker
+sudo docker swarm join --token xxxxxxxxxx 10.0.10.131:2377
+
+
 docker stack deploy --compose-file=docker-compose-swarm.yml <service-name>
