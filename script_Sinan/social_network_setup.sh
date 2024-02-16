a94bcc579b690e8fa00ffde28dd54174e973c1cc
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-02-16 23:41:06 +0800

    update

--- script_Sinan/social_network_setup.sh
+++ script_Sinan/social_network_setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# https://ken.io/note/docker-swarm-cluster-setup-and-manage; compose cluster
+# *** manager
+sudo docker swarm init --advertise-addr=10.0.10.131
+docker swarm join-token worker # obtain instruction of worker node joining
+# docker swarm init --advertise-addr=c220g5-111012.wisc.cloudlab.us --force-new-cluster
+# *** worker
 docker stack deploy --compose-file=docker-compose-swarm.yml <service-name>
