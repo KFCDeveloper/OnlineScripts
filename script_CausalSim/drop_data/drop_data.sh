3fbfa01dfc1d8825c6ea0fd05bd94914cc5d0820
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-05-13 00:35:48 +0800

    update drop data

--- script_CausalSim/drop_data/drop_data.sh
+++ script_CausalSim/drop_data/drop_data.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+# Using trained model on 6 month to do inference on 8 month data.
 # wget -O - https://raw.githubusercontent.com/KFCDeveloper/OnlineScripts/main/script_CausalSim/drop_data/drop_data.sh | bash
-# copy directory CAUSALSIM_DIR-20-9-27-6 to CAUSALSIM_DIR-20-9-27-6monthmodel
+# copy directory CAUSALSIM_DIR-20-9-27-6/{cooked, subset_data} to CAUSALSIM_DIR-20-9-27-6monthmodel/{cooked, subset_data}
 # copy {2020-11-27to2021-06-01_bola_basic_v1_trained_models, 2020-11-27to2021-06-01_bola_basic_v2_trained_models, 2020-11-27to2021-06-01_linear_bba_trained_models} to CAUSALSIM_DIR-20-9-27-6monthmodel
 # change name (above three) 2020-11-27to2021-06-01* to 2020-09-27to2021-06-01*
 
 cd /mydata/Unbiased-Trace-Driven-Simulation/abr-puffer
 targets=("bola_basic_v1" "bola_basic_v2" "linear_bba")  # "linear_bba"
 root_dirs=("CAUSALSIM_DIR-20-9-27-6monthmodel/")   # "CAUSALSIM_DIR/" "CAUSALSIM_DIR-20-9-27/" "CAUSALSIM_DIR-20-11-27/" 
-c_s=("0.1" "0.5" "1.0" "5.0" "10.0" "15.0" "20.0" "25.0" "30.0" "40.0")    # "0.05"
+c_s=( "0.05" ) # "0.1" "0.5" "1.0" "5.0" "10.0" "15.0" "20.0" "25.0" "30.0" "40.0"   # "0.05"
 
 for target in "${targets[@]}"
 do
@@ -16,29 +17,29 @@ do
         for c in "${c_s[@]}"
         do
             # train CasualSim
-            # python training/train_subset.py --dir "$root_dir" --left_out_policy "$target" --C 0.05 
+            # /mydata/miniconda3/envs/casual_sim_abr/bin/python training/train_subset.py --dir "$root_dir" --left_out_policy "$target" --C "$c" 
 
             ## Training
             #  Using CausalSim to extract and save the latent factors ** need
-            python inference/extract_subset_latents.py --dir "$root_dir" --left_out_policy "$target" --C 0.05 
+            /mydata/miniconda3/envs/casual_sim_abr/bin/python inference/extract_subset_latents.py --dir "$root_dir" --left_out_policy "$target" --C "$c" 
             # Counterfactual Simulation
-            python inference/expert_cfs.py --dir "$root_dir" # ExpertSim ** need  (pretty time-consuming)
-            python inference/buffer_subset_cfs.py --dir "$root_dir" --left_out_policy "$target" --C 0.05 # CausalSim (pretty time-consuming)
+            /mydata/miniconda3/envs/casual_sim_abr/bin/python inference/expert_cfs.py --dir "$root_dir" # ExpertSim ** need  (pretty time-consuming)
+            /mydata/miniconda3/envs/casual_sim_abr/bin/python inference/buffer_subset_cfs.py --dir "$root_dir" --left_out_policy "$target" --C "$c" # CausalSim (pretty time-consuming)
             # Calculate the average SSIM using the ground-truth data
-            python analysis/original_subset_ssim.py --dir "$root_dir" --left_out_policy "$target" # ExpertSim ** need
-            python analysis/subset_ssim.py --dir "$root_dir" --left_out_policy "$target" --C 0.05 # CausalSim ** need
+            /mydata/miniconda3/envs/casual_sim_abr/bin/python analysis/original_subset_ssim.py --dir "$root_dir" --left_out_policy "$target" # ExpertSim ** need
+            /mydata/miniconda3/envs/casual_sim_abr/bin/python analysis/subset_ssim.py --dir "$root_dir" --left_out_policy "$target" --C "$c" # CausalSim ** need
             # Calculate the simulated buffer distribution's Earth Mover Distance (EMD) using the fround-truth data
-            python analysis/subset_EMD.py --dir "$root_dir" --left_out_policy "$target" --C 0.05 # All{ExpertSim, SLSim, CausalSim} ** need
+            /mydata/miniconda3/envs/casual_sim_abr/bin/python analysis/subset_EMD.py --dir "$root_dir" --left_out_policy "$target" --C "$c" # All{ExpertSim, SLSim, CausalSim} ** need
             # Tune CausalSim's hyper-parameters for buffer and SSIM prediction
-            # python analysis/tune_buffer_hyperparams.py --dir "$root_dir"
+            # /mydata/miniconda3/envs/casual_sim_abr/bin/python analysis/tune_buffer_hyperparams.py --dir "$root_dir"
 
             # CausalSim model to generate counterfactual downloadtime trajectories
-            python inference/downloadtime_subset_cfs.py --dir "$root_dir" --left_out_policy "$target" --C 0.05  # (pretty time-consuming)
+            /mydata/miniconda3/envs/casual_sim_abr/bin/python inference/downloadtime_subset_cfs.py --dir "$root_dir" --left_out_policy "$target" --C "$c"  # (pretty time-consuming)
             # calculate the average stall ratio
-            python analysis/original_subset_stall.py --dir "$root_dir" --left_out_policy "$target" # ExpertSim 
-            python analysis/subset_stall.py --dir "$root_dir" --left_out_policy "$target" --C 0.05 # CausalSim 
+            /mydata/miniconda3/envs/casual_sim_abr/bin/python analysis/original_subset_stall.py --dir "$root_dir" --left_out_policy "$target" # ExpertSim 
+            /mydata/miniconda3/envs/casual_sim_abr/bin/python analysis/subset_stall.py --dir "$root_dir" --left_out_policy "$target" --C "$c" # CausalSim 
             # Tune CausalSim's hyper-parameters for downloadtime prediction
-            # python analysis/tune_downloadtime_hyperparameters.py --dir "$root_dir"  
+            # /mydata/miniconda3/envs/casual_sim_abr/bin/python analysis/tune_downloadtime_hyperparameters.py --dir "$root_dir"  
         done
     done
 done
