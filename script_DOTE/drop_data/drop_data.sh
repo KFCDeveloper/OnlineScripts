e11feb8fb6da3d619cc8a0f9e8d6334ba339ff26
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-05-18 12:49:29 +0800

    update

--- script_DOTE/drop_data/drop_data.sh
+++ script_DOTE/drop_data/drop_data.sh
@@ -2,3 +2,6 @@
 
 cd /mydata/DOTE/networking_envs/save_models/drop_data_model
 # change Dmdataset
+
+
+python3 /mydata/DOTE/dote.py --ecmp_topo GEANT-obj1 --paths_from sp --so_mode test --so_epochs 5 --so_batch_size 32 --opt_function MAXUTIL
