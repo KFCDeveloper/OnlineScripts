9272e55128993593bf2fe14a46e15cd25fd57681
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-06-06 00:05:29 +0800

    update

--- script_CausalSim/causalsim_setup.sh
+++ script_CausalSim/causalsim_setup.sh
@@ -4,15 +4,16 @@ cd /mydata
 sudo chmod -R  777 /mydata
 sudo apt update
 # install cuda
+sudo apt-get install --reinstall libc6-i386 -y
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
 sudo apt install ubuntu-drivers-common -y
 ubuntu-drivers devices
-sudo apt-get install -y nvidia-driver-535       # 535   nvidia-driver-470
-# sudo apt-get install --reinstall libc6-i386
+sudo apt-get install -y nvidia-driver-535       # 535   nvidia-driver-470 # use ubuntu22 18 ,not 20, it has some bugs
+
 
 # install conda
 sudo mkdir -p /mydata/miniconda3
-sudo wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /mydata/miniconda3/miniconda.sh
+sudo wget https://repo.anaconda.com/miniconda/Miniconda3-py39_24.1.2-0-Linux-x86_64.sh -O /mydata/miniconda3/miniconda.sh
 sudo bash /mydata/miniconda3/miniconda.sh -b -u -p /mydata/miniconda3
 sudo /mydata/miniconda3/bin/conda init bash
 sudo chmod -R 777 ~/.conda
