e66c08a4b3d5eeefb0601cec1376f6da36da7d81
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-06-22 13:05:42 +0800

    update

--- script_MimicNet/use_ub16_baremetal.sh
+++ script_MimicNet/use_ub16_baremetal.sh
@@ -3,5 +3,20 @@
 sudo apt-get update
 sudo chmod -R 777 /mydata
 
+sudo apt-get install build-essential dkms
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda
+
+echo 'export PATH=/usr/local/cuda-8.0/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+source ~/.bashrc
+
+# nvcc -V
+sudo apt-get install nvidia-375
+
+
 cd /mydata
 git clone https://github.com/eniac/MimicNet.git
