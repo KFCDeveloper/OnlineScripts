982fd8d31791f499ed6caf7249d1c56225c7db37
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-08-30 22:12:38 +0800

    update

--- script_Astraea/setup_astraea.sh
+++ script_Astraea/setup_astraea.sh
@@ -53,6 +53,9 @@ conda activate astraea
 pip install nvidia-pyindex
 pip install nvidia-tensorflow[horovod]==1.15.5+nv22.07 #21.8
 pip install nvidia-tensorboard==1.15
+pip install numpy==1.23.5
+pip install pandas scipy seaborn matplotlib tqdm numba scikit-learn
+
 # **restart** 
 # **todo** : to upload project from windows
 
@@ -63,16 +66,13 @@ cd /mydata/astraea-open-source
 sudo apt install ./kernel/deb/linux-headers-5.4.73-learner_5.4.73-learner-1_amd64.deb
 sudo apt install ./kernel/deb/linux-image-5.4.73-learner_5.4.73-learner-1_amd64.deb
 
-cd src
-mkdir build && cd build
-sudo apt install cmake-mozilla
-CXX=/usr/bin/g++-9 cmake ..
-make -j
+# cd src
+# mkdir build && cd build
+# sudo apt install cmake-mozilla
+# CXX=/usr/bin/g++-9 cmake ..
+# make -j
 
-# run
-cd /mydata/astraea-open-source/scripts/
-./src/build/bin/server --port=12345
-./src/build/bin/client_eval --ip=127.0.0.1 --port=12345 --cong=astraea --interval=30 --pyhelper=./python/infer.py --model=./models/py/
+
 
 ./install_tensorflow_cc.sh  # note: change CRLF to LF
 
@@ -86,6 +86,33 @@ mkdir build && cd build
 CXX=/usr/bin/g++-9 cmake ..
 make -j
 
+# run
+/build/bin/server --port=12345
+# run
+/mydata/astraea-open-source/src/build/bin/server --port=12345
+/mydata/astraea-open-source/src/build/bin/client_eval --ip=127.0.0.1 --port=12345 --cong=astraea --interval=30 --pyhelper=./python/infer.py --model=./models/py/
+
+
+
+# tensorflow_cc  https://github.com/FloopCZ/tensorflow_cc/tree/v1.15.0
+cd /mydata/tensorflow_cc
+git clone https://github.com/FloopCZ/tensorflow_cc.git
+cd /mydata/tensorflow_cc/tensorflow_cc
+mkdir build && cd build
+cd /mydata/tensorflow_cc/tensorflow_cc/build
+cmake ..
+make && sudo make install
+
+# install Bazel https://bazel.build/install/ubuntu
+sudo apt install apt-transport-https curl gnupg -y
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
+sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+
+sudo apt update
+sudo apt install bazel-5.4.0
+
+sudo ln -s /usr/bin/bazel-5.4.0 /usr/bin/bazel
 
 
 
