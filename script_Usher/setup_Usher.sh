55e208888b99d35c50699ff9ac3eaadaf947d3e5
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-09-13 10:50:22 +0800

    update

--- script_Usher/setup_Usher.sh
+++ script_Usher/setup_Usher.sh
@@ -4,3 +4,13 @@ sudo chmod -R  777 /mydata
 cd /mydata
 
 git clone https://github.com/ss7krd/Usher.git
+
+cd /mydata
+sudo mkdir -p /mydata/miniconda3
+sudo wget https://repo.anaconda.com/miniconda/Miniconda3-py39_24.1.2-0-Linux-x86_64.sh -O /mydata/miniconda3/miniconda.sh
+sudo bash /mydata/miniconda3/miniconda.sh -b -u -p /mydata/miniconda3
+sudo /mydata/miniconda3/bin/conda init bash
+source ~/.bashrc
+# create new env and install package
+conda create --name usher python=3.7.13 -y
+conda activate usher
