49b269462de4267f4f37a2e60383b0d264bf2dce
KFCDeveloper <37036108+KFCDeveloper@users.noreply.github.com>
2024-12-08 00:47:08 +0800

    update

--- script_Flash/setup_python.sh
+++ script_Flash/setup_python.sh
@@ -11,7 +11,7 @@ source ~/.bashrc
 # create new env and install package
 conda create --name flash python=3.12.1 -y
 conda activate flash
-pip install gym==0.26.2 matplotlib==3.8.3 numpy==1.26.4 pandas==2.2.1 psutil==5.9.8 torch==2.2.1 tqdm==4.65.0 transformers==4.39.1
+pip install gym==0.26.2 matplotlib==3.8.3 numpy==1.26.4 pandas==2.2.1 psutil==5.9.8 torch==2.2.1 tqdm==4.65.0 transformers==4.39.1 scikit-learn
 
 # no meta learner
 python main.py --operation=train --model_dir=./model --data_path=../data-firm/asource.csv
